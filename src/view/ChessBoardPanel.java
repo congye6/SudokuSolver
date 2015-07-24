96f345420b15cfe3574c9613fb32377a5a070a4b
congye6 <244053679@qq.com>
2015-07-24 18:47:57 +0800

    Revert "Revert "7.24.1""
    This reverts commit 0ea5f5784a9fcf92a971fa72e7334d9c508d2533.

--- src/view/ChessBoardPanel.java
+++ src/view/ChessBoardPanel.java
@@ -6,12 +6,23 @@ import java.awt.Image;
 import javax.swing.*;
 
 public class ChessBoardPanel extends JPanel{
-	static final int HEIGHT=360;
-	static final int START=50;
+	static final int HEIGHT_OF_HEAD=50;
+	
+	static final int HEIGHT_OF_BODY=360;
+	
 	static final int SIZE=40;
 	
 	@Override
 	public void paintComponent(Graphics g){
+		//背景
+		Image head=Images.HEAD;
+		Image background=Images.BACKGROUND;
+		g.drawImage(background, 0, 0, null);
+		g.drawImage(head,0,0, null);
 		
+		
+		//操作面板
+		Image oper9=Images.OPERATION[9];
+		g.drawImage(oper9, 0, HEIGHT_OF_HEAD+HEIGHT_OF_BODY, null);
 	}
 }
